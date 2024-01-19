419f9a3ca5081e591922578cf54ef25ab4f470fa
Traftmine <leo.royer.1@etu.u-bordeaux.fr>
2024-01-19 22:42:22 +0100

    TP1

---
+++ backend/src/main/java/pdl/backend/ImageController.java
@@ -0,0 +1,89 @@
+package pdl.backend;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@RestController
+public class ImageController {
+
+  @Autowired
+  private ObjectMapper mapper;
+
+  private final ImageDao imageDao;
+
+  @Autowired
+  public ImageController(ImageDao imageDao) {
+    this.imageDao = imageDao;
+  }
+
+  @RequestMapping(value = "/images/{id}", method = RequestMethod.GET, produces = MediaType.IMAGE_JPEG_VALUE)
+  public ResponseEntity<?> getImage(@PathVariable("id") long id) {
+    Optional<Image> imageOptional = imageDao.retrieve(id); // On recupère l'image associé à l'ID depuis le DAO
+    if (imageOptional.isPresent()) { // On check si l'image existe
+        Image image = imageOptional.get(); // On recupère l'objet image depuis optional
+        return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(image.getData()); // Code 200 avec l'image
+    } else {
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND); // Code 404 si pas trouvé
+    }
+  }
+
+  @RequestMapping(value = "/images/{id}", method = RequestMethod.DELETE)
+  public ResponseEntity<?> deleteImage(@PathVariable("id") long id) {
+    Optional<Image> imageOptional = imageDao.retrieve(id);
+    if (imageOptional.isPresent()) {
+        Image image = imageOptional.get(); // Supprimer l'image à partir du DAO
+        imageDao.delete(image);
+        return new ResponseEntity<>(HttpStatus.OK); // Renvoie Code 200 si bien supprimé
+    } else {
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND); // Code 404 si pas trouvé
+    }
+  }
+
+  @RequestMapping(value = "/images", method = RequestMethod.POST)
+  public ResponseEntity<?> addImage(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
+      try {
+        byte[] fileContent = file.getBytes(); // Lire le contenu du fichier de la requête multipart
+        Image newImage = new Image(file.getOriginalFilename(), fileContent); // Créer une nouvelle image à partir du contenu du fichier
+        imageDao.create(newImage);
+        return new ResponseEntity<>(HttpStatus.CREATED); // Code 201 si l'ajout réussi
+      } catch (IOException e) {
+        e.printStackTrace();
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+      }
+  }
+
+  @RequestMapping(value = "/images", method = RequestMethod.GET, produces = "application/json; charset=UTF-8")
+  @ResponseBody
+  public ArrayNode getImageList() {
+    ArrayNode nodes = mapper.createArrayNode(); // Récupérer la liste des images à partir du DAO
+    List<Image> imageList = imageDao.retrieveAll();
+
+    for (Image image : imageList) { // Ajouter les détails des images à l'objet ArrayNode
+        ObjectNode node = mapper.createObjectNode();
+        node.put("id", image.getId());
+        node.put("name", image.getName());
+        nodes.add(node);
+    }
+
+    return nodes;
+  }
+
+}
---
+++ backend/src/test/java/pdl/backend/ImageControllerTests.java
@@ -0,0 +1,90 @@
+package pdl.backend;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.MediaType;
+
+import javax.swing.text.AbstractDocument.Content;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestMethodOrder(OrderAnnotation.class)
+public class ImageControllerTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@BeforeAll
+	public static void reset() {
+  	// reset Image class static counter
+  	ReflectionTestUtils.setField(Image.class, "count", Long.valueOf(0));
+	}
+
+	@Test
+	@Order(1)
+	public void getImageListShouldReturnSuccess() throws Exception {
+		this.mockMvc.perform(get("/images")).andExpect(status().isOk()).andExpect(content().string(containsString("[{\"id\":0,\"name\":\"logo.jpg\"}]")));
+	}
+
+	@Test
+	@Order(2)
+	public void getImageShouldReturnNotFound() throws Exception {
+		this.mockMvc.perform(get("/images/1")).andExpect(status().isNotFound());
+	}
+
+	@Test
+	@Order(3)
+	public void getImageShouldReturnSuccess() throws Exception {
+		this.mockMvc.perform(get("/images/0")).andExpect(status().isOk());
+	}
+
+	@Test
+	@Order(4)
+	public void deleteImagesShouldReturnMethodNotAllowed() throws Exception {
+		this.mockMvc.perform(delete("/images")).andExpect(status().isMethodNotAllowed());
+	}
+
+	@Test
+	@Order(5)
+	public void deleteImageShouldReturnNotFound() throws Exception {
+		this.mockMvc.perform(delete("/images/1")).andExpect(status().isNotFound());
+	}
+
+	@Test
+	@Order(6)
+	public void deleteImageShouldReturnSuccess() throws Exception {
+		this.mockMvc.perform(delete("/images/0")).andExpect(status().isOk());
+	}
+
+	@Test
+	@Order(7)
+	public void createImageShouldReturnSuccess() throws Exception {
+		// TODO
+	}
+
+	@Test
+	@Order(8)
+	public void createImageShouldReturnUnsupportedMediaType() throws Exception {
+		// TODO
+	}
+
+}
